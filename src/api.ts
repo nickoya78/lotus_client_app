2b5ff6e3ac907c3ceef6a9cd0bd1a8aa44e00d23
Nicholas Oyagh <you@example.com>
2024-05-08 17:17:03 -0700

    env file added

--- src/api.ts
+++ src/api.ts
@@ -1,8 +1,10 @@
 import axios from 'axios';
 
+const apiUrl = process.env.REACT_APP_API_URL;
+
 export const register = async (email: string, password: string, name?: string) => {
   try {
-    const response = await axios.post('https://lotusbackendserver.org/auth/register', { email, password, name });
+    const response = await axios.post(`${apiUrl}/auth/register`, { email, password, name });
     return response;
   } catch (error) {
     console.error(error);
@@ -12,7 +14,7 @@ export const register = async (email: string, password: string, name?: string) =
 
 export const login = async (email: string, password: string) => {
   try {
-    const response = await axios.post('https://lotusbackendserver.org/auth/login', { email, password });
+    const response = await axios.post(`${apiUrl}/auth/login`, { email, password });
     return response;
   } catch (error) {
     console.error((error as any).response.data);
@@ -22,7 +24,7 @@ export const login = async (email: string, password: string) => {
 
 export const logout = async () => {
   try {
-    const response = await axios.post('https://lotusbackendserver.org/auth/logout');
+    const response = await axios.post(`${apiUrl}/auth/logout`);
     return response;
   } catch (error) {
     console.error(error);
@@ -32,7 +34,7 @@ export const logout = async () => {
 
 export const resetPassword = async (email: string) => {
   try {
-    const response = await axios.post('https://lotusbackendserver.org/auth/resetPassword', { email });
+    const response = await axios.post(`${apiUrl}/auth/resetPassword`, { email });
     return response;
   } catch (error) {
     console.error(error);
@@ -42,10 +44,64 @@ export const resetPassword = async (email: string) => {
 
 export const updatePasswordWithToken = async (email: string, newPassword: string, resetToken: string) => {
   try {
-    const response = await axios.post('https://lotusbackendserver.org/auth/updatePasswordWithToken', { email, newPassword, resetToken });
+    const response = await axios.post(`${apiUrl}/auth/updatePasswordWithToken`, { email, newPassword, resetToken });
     return response;
   } catch (error) {
     console.error(error);
     throw error;
   }
 };
+
+
+// *******************************************************************
+// import axios from 'axios';
+
+// export const register = async (email: string, password: string, name?: string) => {
+//   try {
+//     const response = await axios.post('https://lotusbackendserver.org/auth/register', { email, password, name });
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
+
+// export const login = async (email: string, password: string) => {
+//   try {
+//     const response = await axios.post('https://lotusbackendserver.org/auth/login', { email, password });
+//     return response;
+//   } catch (error) {
+//     console.error((error as any).response.data);
+//     throw error;
+//   }
+// };
+
+// export const logout = async () => {
+//   try {
+//     const response = await axios.post('https://lotusbackendserver.org/auth/logout');
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
+
+// export const resetPassword = async (email: string) => {
+//   try {
+//     const response = await axios.post('https://lotusbackendserver.org/auth/resetPassword', { email });
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
+
+// export const updatePasswordWithToken = async (email: string, newPassword: string, resetToken: string) => {
+//   try {
+//     const response = await axios.post('https://lotusbackendserver.org/auth/updatePasswordWithToken', { email, newPassword, resetToken });
+//     return response;
+//   } catch (error) {
+//     console.error(error);
+//     throw error;
+//   }
+// };
